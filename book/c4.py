e54139795adcee9ac1417e3db8e896e75d0f6c17
Alvin-Lo0729 <ak47237496@gmail.com>
2025-12-11 00:33:22 +0800

    string test

---
+++ book/c4.py
@@ -0,0 +1,23 @@
+
+
+letter='a'
+vowels_string='aeiou'
+print("is letter a vowels_string?")
+print(letter in vowels_string)
+
+vowel_set={'a', 'e', 'i', 'o', 'u'}
+print("is letter a vowel_set?")
+print(letter in vowel_set)
+
+vowel_list=['a', 'e', 'i', 'o', 'u']
+print("is letter a vowel_list?")
+print(letter in vowel_list)
+
+vowel_tuple=('a', 'e', 'i', 'o', 'u')
+print("is letter a vowel_tuple?")
+print(letter in vowel_tuple)
+
+vowel_dict={'a':1, 'e':2, 'i':3, 'o':4, 'u':5}
+print("is letter a vowel_dict?")
+print(letter in vowel_dict)
+
---
+++ book/c5.py
@@ -0,0 +1,40 @@
+
+strValue=str(55.4)
+
+print(strValue)
+
+integerValue=str(1.0e4)
+print(integerValue)
+
+palindrome="A man,\nA plan,\na canal:\nPanama."
+
+print(palindrome)
+
+
+print('\tabc')
+print('a\tbc')
+print('ab\tc')
+print('abc\t')
+
+strValue1="abcd"+"bbb"
+print(strValue1)
+
+a="Duck"
+b=a
+c="Grey Duck"
+
+print(a ,b,c)
+start="Na "*4+"\n"
+middle ="Hey!"*3+"\n"
+end="Goodbye."
+print(start,middle,end)
+
+letters='abcdefghijklmnopqrstuvwxyz'
+print(letters[0])
+print(letters[1])
+print(letters[2])
+print(letters[-1])
+print(letters[-2])
+
+
+
