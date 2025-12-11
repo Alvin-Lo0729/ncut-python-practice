fd574bf9930abae29ac1c807b2597b914e29589b
Alvin-Lo0729 <ak47237496@gmail.com>
2025-12-12 00:32:08 +0800

    p76

--- book/c5.py
+++ book/c5.py
@@ -1,3 +1,4 @@
+import string
 
 strValue=str(55.4)
 
@@ -36,5 +37,63 @@ print(letters[2])
 print(letters[-1])
 print(letters[-2])
 
+print(letters[0:4])
 
+print(letters[:])
 
+print(letters[20:])
+print(letters[10:])
+print(letters[12:15])
+print(letters[-3:])
+print(letters[18:-3])
+print(letters[-6:-2])
+print("letters[-2:-6]:"+letters[-2:-6])
+
+print("letters[::7]:"+letters[::7])
+
+print("letters[4:20:3]:"+letters[4:20:3])
+
+print("letters[19::4]:"+letters[19::4])
+
+print("letters[:21:5]:"+letters[:21:5])
+
+print("letters[-1::-1]:"+letters[-1::-1])
+
+print("letters[::-1]:"+letters[::-1])
+
+print(len(letters))
+empty=""
+print(len(empty))
+
+tasks="get gloves,get mask,give cat vitamins,call ambulance"
+print(tasks.split(","))
+print(tasks.split())
+
+crypto_list=['Yeti','Bigfoot','Loch Ness Monster']
+crypto_string=','.join(crypto_list)
+
+print("Found and signing book deals:",crypto_string)
+
+setup="a duck goes into a bar..."
+print(setup.replace('duck','marmoset'))
+print(setup)
+print(setup.replace('a','the ',100))
+
+world="   earth   "
+print("world.strip():",world.strip())
+print("world.strip(' '):",world.strip(' '))
+print("world.lstrip():",world.lstrip())
+print("world.rstrip():",world.rstrip())
+print("world.rstrip('!'):",world.strip('!'))
+print("string.whitespace",string.whitespace)
+print("string.punctuation",string.punctuation)
+
+poem="""All that doth flow we cannot liquid name
+Or else would fire and water be the same;
+But that is liquid which is moist and wet
+Fire that property can never get.
+Then 'tis not cold that doth the fire put out
+But 'tis the wet that makes it die no doubt."""
+
+print("poem[:13]:",poem[:13])
+print(len(poem))
