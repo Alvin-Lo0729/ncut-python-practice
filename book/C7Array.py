31a7d44eab46033251cb6bcfe30a99a342f4a7ae
AlvinLo <ak47237496@gmail.com>
2026-07-20 13:00:58 +0800

    feat(Python):

--- book/C7Array.py
+++ book/C7Array.py
@@ -1,3 +1,6 @@
+import copy
+from os.path import split
+
 emtpy_list = []
 
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
@@ -83,3 +86,97 @@ print(marxes)
 popValue=marxes.pop(1)
 print(popValue)
 print(marxes)
+
+work_quotes=['Working hard?','Quick question!','Number one priorities']
+print(work_quotes)
+
+work_quotes.clear()
+print(work_quotes)
+
+marxes=['Groucho', 'Chico', 'Harpo','Zeppo']
+print(marxes.index('Chico'))
+
+simpsons=['Lisa','Bart','Marge','Homer','Bart']
+print(simpsons.index('Bart'))
+
+
+print('Bbb' in simpsons)
+print("Lisa" in simpsons)
+
+marxes=['Groucho','Choico','Harpo','Choico']
+print(marxes.count('Choico'))
+
+print(" | ".join(marxes))
+
+friends=['Harry','Hermione','Ron']
+separator=' * '
+joined=separator.join(friends)
+print(joined)
+
+separated=joined.split(separator)
+print(separated)
+print(separated.__eq__(friends))
+
+
+marxes=['Groucho','Chico','Harpo']
+sorted_marxes=sorted(marxes)
+print("sorted_marxes:",sorted_marxes)
+print("marxes:",marxes)
+
+marxes.sort()
+print(f"marxes:{marxes}")
+
+numbers=[2,1,4.0,3]
+numbers.sort()
+print(numbers)
+numbers=[2,1,4.0,3]
+numbers.sort(reverse=True)
+print(numbers)
+
+
+marxes=['Groucho','Chico','Harpo']
+print(len(marxes))
+
+print('============')
+
+a=[1,2,3]
+print(f'a:{a}')
+b=a
+print(f'b:{b}')
+a[0]='surprise'
+print(f"a:{a}")
+print(f'b:{b}')
+
+print("=========")
+
+a=[1,2,3]
+b=a.copy();
+c=list(a)
+d=a[:]
+
+a[0]='aaaaaa'
+
+print(f'a:{a}')
+print(f'b:{b}')
+print(f'c:{c}')
+print(f'd:{d}')
+
+print("=========")
+
+a=[1,2,[8,9]]
+
+b=copy.deepcopy(a);
+print(f'a:{a}')
+print(f'b:{b}')
+
+a[2][1]=10;
+print(f'a:{a}')
+print(f'b:{b}')
+
+a=[7,2]
+b=[7,2,9]
+
+print(f'a==b:{a==b}')
+print(f'a<=b:{a<=b}')
+print(f'a<b:{a<b}')
+# page113
